c38b7de12d2793de26e5cf3f1cf0157bab0632d0
Chidav <davidnwatarali@gmail.com>
2022-07-25 19:47:01 +0100

    initial commit

---
+++ loops.js
@@ -0,0 +1,19 @@
+
+// Find the first 10 natural numbers
+for(let i = 1; i<11; i++){
+    console.log(i)
+}
+
+// Find the sum of the first 10 natural numbers
+sum = 0
+for(let i = 1; i<11; i++){
+    sum = sum + i
+    console.log(`${i} is the number and this is the sum ${sum}`)
+}
+
+// Find the sum of the given natural numbers
+var sum = 0
+for(let i = 1; i<8; i++){
+    sum = sum + i
+    console.log(sum)
+}
