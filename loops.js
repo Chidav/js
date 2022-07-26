5f3e3bdd12685a211bb29590c9afdac1380cdbe4
Chidav <davidnwatarali@gmail.com>
2022-07-26 12:37:08 +0100

    modified

--- loops.js
+++ loops.js
@@ -16,9 +16,9 @@ for(let i = 1; i<11; i++){
 
 // NO 3
 // Find the sum of the given natural numbers
-i = 7
+n = 7
 var sum = 0
-for(let i = 1; i<8; i++){
+for(let i = 1; i<=n; i++){
     sum = sum + i
     console.log(sum)
 }
@@ -28,20 +28,19 @@ for(let i = 1; i<8; i++){
 // Find the sum of 10 numbers and their average
 sum = 0
 i = 10
+avg = sum/10
 for(let i = 1; i<11; i++){
     sum = sum + i
-    console.log(`${i} is the number and this is the sum ${sum}`)
-    if (i===10){
-        avg = sum / 10
-        console.log(`The average of the added numbers are ${avg}`)
-    }
+    avg = sum / 10
 }
+console.log(sum, avg)
 
 
 // NO 5
 // Find the cube of numbers of the given integer
 i = 5
+cube = i * i * i
 for(let i = 1; i<6; i++){
     cube = i * i * i
-    console.log(`${i} and cube of the 1 is: ${cube}`)
 }
+console.log(`This is the last number ${i} and the cube is: ${cube}`)
